e8b870ef71bc58971df1919bbb6f516e0d21c779
WillianFernandes17 <william.fernandes@totemti.com.br>
2021-03-04 16:52:32 -0400

    atual

--- index.js
+++ index.js
@@ -6,9 +6,35 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const questions = [
     {
         type: 'input',
-        message: 'O que vpcê gosta?',
-        name: 'gosta',
-    }
+        message: 'Qual seu username no Github?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'Nome do repositório no Github',
+        name: 'repositorio',
+    },
+    {
+        type: 'input',
+        message: 'Titulo do projeto',
+        name: 'titulo',
+    },
+    {
+        type: 'input',
+        message: 'Descrição do projeto',
+        name: 'descricao',
+    },
+    {
+        type: 'input',
+        message: 'Url da imagem de capa/preview do projeto',
+        name: 'imagem',
+    },
+    {
+        type: 'input',
+        message: 'Comando do Script para rodar o projeto (start)',
+        name: 'run',
+    },
+
 ]
 
 function writeToFile(filename, data){
@@ -27,7 +53,7 @@ async function init(){
     try {
         const userResponses = await inquirer.prompt(questions)
 
-        const markdown = generateMarkdown()
+        const markdown = generateMarkdown(userResponses)
 
          await writeFileAsync(`README${Math.random(100)}.md`, markdown)
     } catch (error) {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,30 +1,27 @@
-function generateMarkdown(){
+function generateMarkdown(userResponses){
     return `
-![GitHub repo size](https://img.shields.io/github/repo-size/Willian17/CalculatorIF?style=for-the-badge)
-![GitHub language count](https://img.shields.io/github/languages/count/Willian17/CalculatorIF?style=for-the-badge)
-![GitHub forks](https://img.shields.io/github/forks/Willian17/CalculatorIF?style=for-the-badge)
-  <a href="https://github.com/Willian17/weight-history/commits/master">
-    <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/Willian17/weight-history?color=774DD6&style=for-the-badge">
-  </a> 
-    <a href="https://github.com/Willian17/model-complete-readme/stargazers">
-    <img alt="Stargazers" src="https://img.shields.io/github/stars/Willian17/weight-history?style=for-the-badge&logo=github">
-  </a>
-    <a href="https://www.linkedin.com/in/willian-fernandes/">
-      <img alt="Willian Fernandes" src="https://img.shields.io/badge/-WillianFernandes-8257E5?style=for-the-badge&logo=Linkedin&logoColor=white&" />
-   </a>
-  
+![GitHub repo size](https://img.shields.io/github/repo-size/${userResponses.username}/${userResponses.repositorio}?style=for-the-badge)
+![GitHub language count](https://img.shields.io/github/languages/count/${userResponses.username}/${userResponses.repositorio}?style=for-the-badge)
+![GitHub forks](https://img.shields.io/github/forks/${userResponses.username}/${userResponses.repositorio}?style=for-the-badge)
+<a href="https://github.com/${userResponses.username}${userResponses.repositorio}/commits/master">
+<img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/${userResponses.username}/${userResponses.repositorio}?color=774DD6&style=for-the-badge">
+</a> 
+<a href="https://github.com/${userResponses.username}/model-complete-readme/stargazers">
+<img alt="Stargazers" src="https://img.shields.io/github/stars/${userResponses.username}/${userResponses.repositorio}?style=for-the-badge&logo=github">
+</a>
+
 <p align="center">
 
-  <h1 align="center"> Titulo do projeto </h3>
-  
-  <p align="center"><i>Linha adicional de texto informativo sobre o que o projeto faz. Sua introdução deve ter cerca de 2 ou 3 linhas. Não exagere, as pessoas não vão ler.</i></p>
+<h1 align="center"> ${userResponses.titulo} </h3>
+
+<p align="center"><i>${userResponses.descricao}</i></p>
 
 <details>
 <summary>📖 Conteúdo</summary>
 <br />
 
 * [➤ Sobre o projeto](#sobre)
-	* [Funcionalidades e melhorias](#melhorias)
+* [Funcionalidades e melhorias](#melhorias)
 * [➤ Como executar](#executar)
 * [➤ Tecnologias](#tecnologias)
 * [➤ Colaboradores](#colaboradores)
@@ -36,27 +33,15 @@ function generateMarkdown(){
 
 ## :pushpin: Sobre o Projeto <a name="sobre"></a>
 <div>
-   <img src="https://raw.githubusercontent.com/RafaelGoulartB/proffy/master/.github/web-landing.png" width="400px">
-   <img src="https://raw.githubusercontent.com/RafaelGoulartB/proffy/master/.github/web-list.png" width="400px">
+<img src="${userResponses.imagem}" width="400px">
 </div>
-
-
-### :warning: Funcionalidades e melhorias <a name="melhorias"></a>
-
-O projeto ainda está em desenvolvimento e as próximas atualizações serão voltadas nas seguintes tarefas:
-
-- [x] Tarefa 1
-- [x] Tarefa 2
-- [x] Tarefa 3
-- [ ] Tarefa 4
-- [ ] Tarefa 5
 
 [![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#executar)
 
 ## :construction_worker: Como executar <a name="executar"></a>
-bash
+
 # Clone Repository
-$ git clone https://github.com/Willian17/proffy-nlw2.git
+$ git clone https://github.com/${userResponses.username}/${userResponses.repositorio}.git
 
 # Instalar Dependencias
 $ yarn install 
@@ -74,32 +59,32 @@ $ npm start
 
 ## :computer: Tecnologias <a name="tecnologias"></a>
 <ul>
-  <li>Node.js</li>
-  <li>Express.js</li>
-  <li>Typescript</li>
-  <li>React</li>
-  <li>React Native</li>
-  <li>Expo</li>
+<li>Node.js</li>
+<li>Express.js</li>
+<li>Typescript</li>
+<li>React</li>
+<li>React Native</li>
+<li>Expo</li>
 
 </ul>
 
-[![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)]
+![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)
 
 ## 🤝 Colaboradores <a name="colaboradores"></a>
 
 Agradecemos às seguintes pessoas que contribuíram para este projeto:
 
 <table>
-  <tr>
-    <td align="center">
-      <a href="#">
-        <img src="https://avatars2.githubusercontent.com/u/53010824?s=460&u=40f41a9ce155fe1a214f422849ad88fd7f0b9710&v=4" width="100px;" alt="Foto do Willian Fernandes no GitHub"/><br>
-        <sub>
-          <b>Willian Fernandes</b>
-        </sub>
-      </a>
-    </td>
-  </tr>
+<tr>
+<td align="center">
+<a href="#">
+  <img src="https://avatars2.githubusercontent.com/u/53010824?s=460&u=40f41a9ce155fe1a214f422849ad88fd7f0b9710&v=4" width="100px;" alt="Foto do Willian Fernandes no GitHub"/><br>
+  <sub>
+    <b>Willian Fernandes</b>
+  </sub>
+</a>
+</td>
+</tr>
 </table>
 
 [![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#contribuidores)
@@ -123,7 +108,6 @@ Esse projeto está sob licença MIT.
 ## :man_astronaut: Mostre seu apoio 
 
 Dê uma ⭐️ se esse projeto te ajudou!
-
 `;
 }
 
